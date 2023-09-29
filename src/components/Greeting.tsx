d001c3418820d3b71da32cd29642b5e921b1658f
Peeranut Chaturong <annmalaew@Peeranuts-MacBook-Pro.local>
2023-09-29 14:47:52 +0700

    adding

---
+++ src/components/Greeting.tsx
@@ -0,0 +1,12 @@
+import classes from './Greeting.module.css'
+
+const Greeting = () => {
+  return (
+    <div className={classes.card}>
+      <h3>welcome!</h3>
+      <p>annmalaew</p>
+    </div>
+  )
+}
+
+export default Greeting
