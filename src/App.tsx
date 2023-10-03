e70cecd4559dd4ad897e1957260765b1bb8db5b1
Peeranut Chaturong <annmalaew@Peeranuts-MacBook-Pro.local>
2023-10-03 11:21:10 +0700

    demo states

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Greeting name="Ann" greetingMsg="Hello!" isLoggedIn={true} />
+      <Greeting name="Ann" isLoggedIn={true} />
       <div className="feed-container">
         {posts.map((post) => {
           return <Post key={post.id} post={post} />
--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,17 +1,25 @@
+import { useState } from 'react'
 import classes from './Greeting.module.css'
 
 interface IGreetingProps {
   name: string
-  greetingMsg: string
   isLoggedIn: boolean
 }
 
-const Greeting = ({ name, greetingMsg, isLoggedIn }: IGreetingProps) => {
+const Greeting = ({ name, isLoggedIn }: IGreetingProps) => {
+  const [greetingMsg, setGreetingmsg] = useState<string>('Welcome!')
+
+  const handleClick = () => {
+    setGreetingmsg('Hello!')
+  }
   return (
-    <div className={classes.card}>
-      <h3>{greetingMsg}</h3>
-      <p>{isLoggedIn ? name : 'Unknown'}</p>
-    </div>
+    <>
+      <div className={classes.card}>
+        <h3>{greetingMsg}</h3>
+        <p>{isLoggedIn ? name : 'Unknown'}</p>
+      </div>
+      <button onClick={handleClick}>change greeting message</button>
+    </>
   )
 }
 
