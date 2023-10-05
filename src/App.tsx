7f424fc9ed5f4fbce11be3339e0e6f88bddf877a
Peeranut Chaturong <annmalaew@Peeranuts-MacBook-Pro.local>
2023-10-05 10:21:50 +0700

    demo/useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -3,66 +3,49 @@ import './App.css'
 import Greeting from './components/Greeting'
 import Navbar from './components/Navbar'
 import Post from './components/Post'
-import { PostDTO } from './components/types/dto'
-
-const initialPosts: PostDTO[] = [
-  {
-    id: 1,
-    userId: 1,
-    title: "Let's learn React!",
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  },
-  {
-    id: 2,
-    userId: 2,
-    title: 'How to install Node.js',
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  },
-  {
-    id: 3,
-    userId: 3,
-    title: 'Basic HTML',
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  },
-]
+import usePosts from './hooks/usePost'
 
 function App() {
-  const [posts, setPosts] = useState<PostDTO[]>(initialPosts)
-  const [newTitle, setNewtitle] = useState<string>('')
+  const { posts, isLoading, isSubmitting, createPost } = usePosts()
+  const [newTitle, setNewTitle] = useState<string>('')
   const [newBody, setNewBody] = useState<string>('')
 
-  const handSubmit = (e: FormEvent) => {
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
 
-    const currentPosts = [...posts]
+    try {
+      await createPost(newTitle, newBody)
 
-    currentPosts.push({
-      id: Math.floor(Math.random() * 1000),
-      userId: Math.floor(Math.random() * 1000),
-      title: newTitle,
-      body: newBody,
-    })
-    setPosts(currentPosts)
-
-    setNewtitle('')
-    setNewBody('')
+      setNewTitle('')
+      setNewBody('')
+    } catch (err) {
+      console.error(err)
+    }
   }
+
+  if (isLoading) return <h1>Loading...</h1>
 
   return (
     <div className="App">
       <Navbar />
-      <Greeting name="Ann" isLoggedIn={true} />
-      <form onSubmit={handSubmit}>
+      <Greeting name="Bun" isLoggedIn={true} />
+
+      <form onSubmit={handleSubmit}>
         <label>Title</label>
-        <input type="text" value={newTitle} onChange={(e) => setNewtitle(e.target.value)} required></input>
+        <input type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} required />
         <label>Body</label>
-        <input type="text" value={newBody} onChange={(e) => setNewBody(e.target.value)} required></input>
-        <button type="submit">Submit</button>
+        <input type="text" value={newBody} onChange={(e) => setNewBody(e.target.value)} required />
+
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? 'Submitting...' : 'Submit'}
+        </button>
       </form>
+
       <div className="feed-container">
-        {posts.map((post) => {
-          return <Post key={post.id} post={post} />
-        })}
+        {posts &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} />
+          })}
       </div>
     </div>
   )
